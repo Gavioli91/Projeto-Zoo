a4a1d0bc0ce367fd4a814e59a12327380e3a2d64
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2022-06-29 19:35:35 -0300

    Requisitos 2 e 3...

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(bicho, age) {
-const nameBicho = data.species.find((name) => name.name === bicho);
-return nameBicho.residents.every((ageBicho) => ageBicho.age >= age);
+  const nameBicho = data.species.find((name) => name.name === bicho);
+  return nameBicho.residents.every((ageBicho) => ageBicho.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
