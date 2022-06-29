b174234e777d392042ccac1942dc1496420a47b1
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2022-06-29 16:38:47 -0300

    Requisito1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const {species} = data
+
+function getSpeciesByIds(...ids) {
+ if (!ids.length) return [];
+  return species.filter(({id}) => ids.includes(id))
+ }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
