914d3ae40700dc10f4ea6b110d1409ebb9d35d56
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2022-06-29 16:45:21 -0300

    Requisito 1

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,6 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,11 +1,11 @@
 const data = require('../data/zoo_data');
 
-const {species} = data
+const { species } = data;
 
 function getSpeciesByIds(...ids) {
- if (!ids.length) return [];
-  return species.filter(({id}) => ids.includes(id))
- }
+if (!ids.length) return [];
+  return species.filter(({ id }) => ids.includes(id));
+}
 
 console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
