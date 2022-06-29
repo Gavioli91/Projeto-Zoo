bca462d5af1fffb7663c6ec3ea12cff2829109ce
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2022-06-29 19:13:42 -0300

    Requisitos 2 e 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,6 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-}
+function getAnimalsOlderThan(bicho, age) {
+    const nameBicho = data.species.find((name) => name.name === bicho);
+    return nameBicho.residents.every((ageBicho) => ageBicho.age >= age);
+  }
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return data.employees.find(({ firstName, lastName }) => firstName === employeeName
+   || lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
