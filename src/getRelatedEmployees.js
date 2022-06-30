db3cc33aca0334d30b85d98e21fbc61b87c6dd61
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2022-06-30 11:08:25 -0300

    4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
-  return data.employees.some((gerente) => gerente.managers.includes(id))
+  return data.employees.some((gerente) => gerente.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
@@ -10,7 +10,7 @@ function getRelatedEmployees(managerId) {
   if (isManager(managerId)) {
     const manager = data.employees.filter((callback) =>
       callback.managers.some((id) => id === managerId));
-    return manager.map((employee) => `${ employee.firstName } ${ employee.lastName }`);
+    return manager.map((employee) => `${employee.firstName} ${employee.lastName}`);
   }
   if (!isManager(managerId)) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
