60e9d9b0ee86deaf52452d5559a83b212822d377
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2022-06-30 18:24:58 -0300

    atualizações...

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,3 +1,9 @@
 const data = require('../data/zoo_data');
 
+function countEntrants(entrants) {
+  // seu código aqui
+}
+  function calculateEntry(entrants) {
+    // seu código aqui
+  }
 module.exports = { calculateEntry, countEntrants };
