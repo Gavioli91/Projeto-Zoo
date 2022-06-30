2a532d131b631026a26cf47e366102f843d21c5c
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2022-06-30 15:37:29 -0300

    Requisito 5 de novo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,18 +1,15 @@
 const data = require('../data/zoo_data');
 
-
   const countAnimals = (animal) => {
-    const vazio = {};
+  const vazio = {};
     if (!animal) {
-      data.species.forEach((bicho) => {
-        vazio[bicho.name] = bicho.residents.length;
+    data.species.forEach((bicho) => {
+      vazio[bicho.name] = bicho.residents.length;
       });
       return vazio;
-    }
+      }
     if (!animal.sex) return data.species.find((animais) => animais.name === animal.specie).residents.length;
     return data.species.find((animais) => animais.name === animal.specie)
-      .residents.filter((animais) => animais.sex === animal.sex).length;
-  };
-
-
+    .residents.filter((animais) => animais.sex === animal.sex).length;
+      };
 module.exports = countAnimals;
