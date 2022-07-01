4c696f154da40a2eec15c019eb32980c7f18ee88
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2022-07-01 14:54:50 -0300

    5, 6, 7 e 10 refatoradas

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -9,8 +9,7 @@ const countAnimals = (animal) => {
     return vazio;
   }
   if (!animal.sex) {
-    return data.species.find((animais) =>
-    animais.name === animal.specie).residents.length;
+    return data.species.find((animais) => animais.name === animal.specie).residents.length;
   }
   return data.species.find((animais) => animais.name === animal.specie)
     .residents.filter((animais) => animais.sex === animal.sex).length;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,10 +2,8 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
-const idAnimal = data.employees.find((employee) => 
-    employee.id === id).responsibleFor[0];
-const animalsResidents = data.species.find((animal) => 
-    animal.id === idAnimal).residents;
+  const idAnimal = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const animalsResidents = data.species.find((animal) => animal.id === idAnimal).residents;
   return Object.values(animalsResidents.reduce((acc, curr) => {
     if (curr.age >= acc.age) {
       return curr;
