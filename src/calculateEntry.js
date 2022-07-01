290e2107ebde0537b80666e40dcf50e03eaa2021
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2022-07-01 16:32:25 -0300

    Requisitos 8, 12 e 13

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,9 +1,22 @@
 const data = require('../data/zoo_data');
 
+const priceChildren = data.prices.child;
+const priceAdult = data.prices.adult;
+const priceSenior = data.prices.senior;
+const entrantsToday = [];
 function countEntrants(entrants) {
-  // seu código aqui
+  const children = entrants.filter((child) => child.age < 18).length;
+  const adults = entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length;
+  const seniors = entrants.filter((senior) => senior.age >= 50).length;
+  return { child: children, adult: adults, senior: seniors };
 }
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  return (child * data.prices.child) + (adult * data.prices.adult) + (senior * data.prices.senior);
 }
+
+module.exports = { calculateEntry, countEntrants };
 module.exports = { calculateEntry, countEntrants };
