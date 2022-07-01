dd818133dc4540cf30e4d650b99f08ea9a9f34e6
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2022-07-01 10:23:12 -0300

    R10

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 function countEntrants(entrants) {
   // seu código aqui
 }
-  function calculateEntry(entrants) {
-    // seu código aqui
-  }
+function calculateEntry(entrants) {
+  // seu código aqui
+}
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,17 +1,7 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  const vazio = {};
-  if (!animal) {
-    data.species.forEach((bicho) => {
-      vazio[bicho.name] = bicho.residents.length;
-    });
-    return vazio;
-  }
-  if ( !animal.sex) return data.species.find((animais) =>
-    animais.name === animal.specie).residents.length;
-  return data.species.find((animais) => animais.name === animal.specie)
-    .residents.filter((animais) => animais.sex === animal.sex).length;
-};
+function countAnimals(animal) {
+  // seu código aqui
+}
 module.exports = countAnimals;
 
